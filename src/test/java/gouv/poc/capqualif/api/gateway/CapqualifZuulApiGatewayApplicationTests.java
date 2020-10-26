a360abff68a5f493e3e2c746d34e2edb09f165c7
Elisabeth Fainstein <elisabeth.fainstein@developpement-durable.gouv.fr>
2020-10-26 10:19:07 +0100

    first commit

---
+++ src/test/java/gouv/poc/capqualif/api/gateway/CapqualifZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package gouv.poc.capqualif.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapqualifZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
